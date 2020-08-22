f61bd4a868c0174b5a3ba9c1daa176e755b2b068
Jorge <jhessle04@gmail.com>
2020-08-22 00:28:24 -0300

    update

---
+++ src/views/js/main.js
@@ -0,0 +1,9 @@
+(function($){
+
+    document.getElementById("cart-info").addEventListener("click", function() {
+          const cart = document.getElementById("cart");
+          cart.classList.toggle("show-cart");
+          console.log(cart);
+      });
+  
+  })(jQuery);
